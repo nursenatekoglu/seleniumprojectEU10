c8a7f507d7a856b716cb35f4f530deb54287eb4c
Nursena Tekoglu <nursena.tekoglu@gmail.com>
2022-12-20 22:37:29 +0300

    basic navigations with selenium created

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -1,2 +1,56 @@
-package com.cydeo.tests.day1_selenium_intro;public class BsicNavigations {
+package com.cydeo.tests.day1_selenium_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigations {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //1-Setting up the web driver manager
+        WebDriverManager.chromedriver().setup();
+
+        //2-Create instance of the Selenium Web Driver
+        //This the line opening an empty browser
+        WebDriver driver = new ChromeDriver();
+
+        //3- Go to "https://www.tesla.com"
+        driver.get("https://www.tesla.com");
+
+        //stop the code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate back
+        driver.navigate().back();
+
+        //stop the code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate forward
+        driver.navigate().forward();
+
+        //stop the code execution for 3 seconds
+        Thread.sleep(3000);
+
+        // use selenium to navigate refresh
+        driver.navigate().refresh();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
 }
