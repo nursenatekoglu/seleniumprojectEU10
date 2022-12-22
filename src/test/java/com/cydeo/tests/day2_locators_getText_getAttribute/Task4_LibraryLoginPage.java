3ac0c2f6f631cd6acb6f99b07df1a76fa2595a4b
Nursena Tekoglu <nursena.tekoglu@gmail.com>
2022-12-22 20:02:27 +0300

    day 2 task 4 - library login page practice updated

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/Task4_LibraryLoginPage.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/Task4_LibraryLoginPage.java
@@ -1,13 +1,18 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task4_LibraryLoginPage {
     public static void main(String[] args) {
 
         // TC #4: Library verifications
+        // Locate username input box using “className” locator
+        // Locate password input box using “id” locator
+        // Locate Sign in button using “tagName” locator
 
         // 1. Open Chrome browser
         WebDriverManager.chromedriver().setup();
@@ -15,17 +20,23 @@ public class Task4_LibraryLoginPage {
 
         driver.manage().window().maximize();
 
-        // 2. Go to http://library2.cybertekschool.com/login.html
-        driver.get("http://library2.cybertekschool.com/login.html ");
+        // 2. Go to https://library2.cybertekschool.com/login.html
+        driver.get("https://library2.cybertekschool.com/login.html ");
 
         // 3. Enter username: “incorrect@email.com”
+        WebElement userNameInput = driver.findElement(By.className("form-control"));
+        userNameInput.sendKeys("incorrect@email.com");
 
         // 4. Enter password: “incorrect password”
+        WebElement passwordInput = driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect password");
 
-        // 5. Verify: visually “Sorry, Wrong Email or Password” displayed
-        // PS: Locate username input box using “className” locator
-        // Locate password input box using “id” locator
-        // Locate Sign in button using “tagName” locator
+        // 5. Click to Sign In  button
+        WebElement signInButton = driver.findElement(By.tagName("button"));
+        signInButton.click();
+
+        // 6. Verify: visually “Sorry, Wrong Email or Password” displayed
+
 
 
 
