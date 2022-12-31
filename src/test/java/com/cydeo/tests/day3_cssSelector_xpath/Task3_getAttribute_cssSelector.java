3db8d1bc580a53d7a83de29c8ec120b9b723ffad
Nursena Tekoglu <nursena.tekoglu@gmail.com>
2022-12-31 23:31:23 +0300

    day 3 task 3 - getAttribute - cssSelector

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/Task3_getAttribute_cssSelector.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class Task3_getAttribute_cssSelector {
+
+    public static void main(String[] args) {
+
+    //TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
+    //1- Open a chrome browser
+    WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+    //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+    //3- Verify “Log in” button text is as expected:
+    //Expected: Log In
+    //
+    //
+    //
+    //PS: Inspect and decide which locator you should be using to locate web
+    //elements.
+    //PS2: Pay attention to where to get the text of this button from
+
+
+
+
+
+    }
+}
